729db878a0f21ef0d54bf918e45a0232a3b11b4f
xueliang.cxl <goliathkaixm@gmail.com>
2014-08-05 11:20:37 +0800

    解析界别

--- src/java/main/com/alibaba/differ/AnalysisLevel.java
+++ src/java/main/com/alibaba/differ/AnalysisLevel.java
@@ -4,28 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum AnalysisLevel {
-	J(1), C(2), M(3);
+    /** Jar */
+    J(1),
+    /** Class */
+    C(2),
+    /** Method */
+    M(3);
 
-	private static Map<String, AnalysisLevel> holder = new HashMap<String, AnalysisLevel>();
+    private static Map<String, AnalysisLevel> holder = new HashMap<String, AnalysisLevel>();
 
-	static {
-		holder.put(J.toString(), J);
-		holder.put(C.toString(), C);
-		holder.put(M.toString(), M);
-	}
+    static {
+        holder.put(J.toString(), J);
+        holder.put(C.toString(), C);
+        holder.put(M.toString(), M);
+    }
 
-	private int weight;
+    private int                               weight;
 
-	AnalysisLevel(int weight) {
-		this.weight = weight;
-	}
+    AnalysisLevel(int weight){
+        this.weight = weight;
+    }
 
-	public static AnalysisLevel getLevel(String analysisLevelStr) {
-		return holder.get(analysisLevelStr);
-	}
+    public static AnalysisLevel getLevel(String analysisLevelStr) {
+        return holder.get(analysisLevelStr);
+    }
 
-	public int getWeight() {
-		return weight;
-	}
+    public int getWeight() {
+        return weight;
+    }
 
 }
